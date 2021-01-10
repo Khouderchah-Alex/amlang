ee6af6f8936c7e801d3cfd60c9210a52530b5a90
Alex Khouderchah <alex.khouderchah@gmail.com>
2021-01-10 11:33:00 -0800

    ast: Impl Display for S-exp objects

--- src/ast.rs
+++ src/ast.rs
@@ -1,3 +1,6 @@
+//! Module for representing S-exps.
+
+use std::fmt;
 
 #[derive(Debug)]
 pub enum Value {
@@ -14,6 +17,99 @@ pub enum Atom {
 
 #[derive(Debug, Default)]
 pub struct Cons {
-    car: Option<Box<Value>>,
-    cdr: Option<Box<Value>>,
+    pub car: Option<Box<Value>>,
+    pub cdr: Option<Box<Value>>,
+}
+
+
+impl fmt::Display for Value {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Value::Atom(atom) => {
+                write!(f, "{}", atom)
+            }
+            Value::Cons(cons) => {
+                if f.alternate() {
+                    write!(f, "{:#}", cons)
+                } else {
+                    write!(f, "{}", cons)
+                }
+            }
+        }
+    }
+}
+
+impl fmt::Display for Atom {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Atom::Integer(i) => {
+                write!(f, "{}", i)
+            }
+            Atom::Float(ff) => {
+                write!(f, "{}f", ff)
+            }
+            Atom::Symbol(s) => {
+                write!(f, "{:}", s)
+            }
+        }
+    }
+}
+
+impl fmt::Display for Cons {
+    /// Note: this does not check for loops and doesn't have a max depth.
+    /// Use the alternate formatting for untrusted S-exps.
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        // Alternate print this with the list shorthand.
+        if f.alternate() {
+            return self.list_fmt(f);
+        }
+
+        let a = match &self.car {
+            Some(val) => val.to_string(),
+            None => "NIL".to_string(),
+        };
+        let b = match &self.cdr {
+            Some(val) => val.to_string(),
+            None => "NIL".to_string(),
+        };
+
+        write!(f, "({} . {})", a, b)
+    }
+}
+
+impl Cons {
+    fn list_fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        // Any list longer than this will simply be suffixed with "..." after these
+        // many elements.
+        const MAX_DISPLAY_LENGTH: usize = 64;
+
+        let mut pos: usize = 0;
+        let mut curr: &Cons = &self;
+        write!(f, "(")?;
+        loop {
+            if pos >= MAX_DISPLAY_LENGTH {
+                write!(f, "...")?;
+                break;
+            }
+
+            match &curr.car {
+                Some(val) => {
+                    write!(f, "{:#}", val)?;
+                },
+                None => { write!(f, "NIL")?; },
+            }
+
+            match &curr.cdr {
+                Some(val) => {
+                    if let Value::Cons(next) = &*val.as_ref() {
+                        curr = &next;
+                        pos += 1;
+                    }
+                    write!(f, " ")?;
+                },
+                None => { break; },
+            };
+        }
+        write!(f, ")")
+    }
 }
