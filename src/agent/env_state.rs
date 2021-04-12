6d1beea72c26efcf7f7310a22e0a97b083728bd1
Alex Khouderchah <alex.khouderchah@gmail.com>
2021-04-12 15:10:00 -0700

    agent: env_state: Construct without needing to pun self NodeId

--- src/agent/env_state.rs
+++ src/agent/env_state.rs
@@ -15,13 +15,10 @@ pub struct EnvState {
 impl EnvState {
     pub fn new() -> Self {
         let mut meta = MetaEnvironment::new();
-        let meta_self = meta.self_node();
-        Self {
-            env: meta.insert_structure(MetaEnvStructure::Env(Box::new(MemEnvironment::new()))),
-            pos: meta_self,
+        let env = meta.insert_structure(MetaEnvStructure::Env(Box::new(MemEnvironment::new())));
+        let pos = EnvState::access_env(&mut meta, env).self_node();
 
-            meta,
-        }
+        Self { env, pos, meta }
     }
 
     pub fn pos(&self) -> NodeId {
@@ -37,7 +34,12 @@ impl EnvState {
     // pub fn teleport(&mut self, portal: Portal)
 
     pub fn env(&mut self) -> &mut EnvObject {
-        match self.meta.node_structure(self.env).unwrap() {
+        EnvState::access_env(&mut self.meta, self.env)
+    }
+
+    // TODO(func) Move to same central location as above.
+    fn access_env(meta: &mut MetaEnvironment, node: NodeId) -> &mut EnvObject {
+        match meta.node_structure(node).unwrap() {
             MetaEnvStructure::Env(env) => env.as_mut(),
             _ => panic!(),
         }
