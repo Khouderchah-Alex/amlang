9da5cb7a0230609ee5d675db538c47cd39342358
Alex Khouderchah <alex.khouderchah@gmail.com>
2021-05-13 10:24:37 -0700

    amlang_agent: Move raw wrappers into separate module

--- src/agent/amlang_agent.rs
+++ src/agent/amlang_agent.rs
@@ -2,6 +2,7 @@ use std::borrow::Cow;
 use std::convert::TryFrom;
 
 use super::agent::Agent;
+use super::amlang_wrappers::*;
 use super::env_state::EnvState;
 use crate::builtins::{add, div, mul, sub};
 use crate::function::{
@@ -24,93 +25,6 @@ impl AmlangAgent {
     pub fn new() -> Self {
         let env_state = EnvState::new();
         Self { env_state }
-    }
-
-    fn env_insert_node_wrapper(&mut self, args: Option<&Sexp>) -> Ret {
-        if args.is_none() {
-            return Err(WrongArgumentCount {
-                given: 0,
-                expected: ExpectedCount::AtLeast(1),
-            });
-        }
-
-        let cons = match args.unwrap() {
-            Sexp::Primitive(primitive) => {
-                return Err(InvalidSexp(primitive.clone().into()));
-            }
-            Sexp::Cons(cons) => cons,
-        };
-
-        let mut iter = cons.iter();
-        let name = if let Ok(symbol) = <&Symbol>::try_from(iter.next()) {
-            symbol
-        } else {
-            return Err(InvalidArgument {
-                given: cons.clone().into(),
-                expected: Cow::Borrowed("symbol"),
-            });
-        };
-
-        let structure = iter.next();
-        if structure.is_some() && iter.next().is_some() {
-            return Err(WrongArgumentCount {
-                given: iter.count() + 3,
-                expected: ExpectedCount::AtMost(2),
-            });
-        }
-
-        Ok(self.env_insert_node(name, structure)?.into())
-    }
-
-    fn env_insert_triple_wrapper(&mut self, args: Option<&Sexp>) -> Ret {
-        if args.is_none() {
-            return Err(WrongArgumentCount {
-                given: 0,
-                expected: ExpectedCount::Exactly(3),
-            });
-        }
-
-        let cons = match args.unwrap() {
-            Sexp::Primitive(primitive) => {
-                return Err(InvalidSexp(primitive.clone().into()));
-            }
-            Sexp::Cons(cons) => cons,
-        };
-
-        fn extract_symbol<'a, I: Iterator<Item = &'a Sexp>>(
-            i: usize,
-            iter: &mut I,
-        ) -> Result<&'a Symbol, EvalErr> {
-            if let Some(elem) = iter.next() {
-                if let Ok(symbol) = <&Symbol>::try_from(elem) {
-                    Ok(symbol)
-                } else {
-                    Err(InvalidArgument {
-                        given: elem.clone().into(),
-                        expected: Cow::Borrowed("symbol"),
-                    })
-                }
-            } else {
-                Err(WrongArgumentCount {
-                    given: i,
-                    expected: ExpectedCount::Exactly(3),
-                })
-            }
-        }
-
-        let mut iter = cons.iter();
-        let subject = extract_symbol(0, &mut iter)?;
-        let predicate = extract_symbol(1, &mut iter)?;
-        let object = extract_symbol(2, &mut iter)?;
-
-        if let Some(_) = iter.next() {
-            return Err(WrongArgumentCount {
-                given: cons.iter().count(),
-                expected: ExpectedCount::Exactly(3),
-            });
-        }
-
-        self.env_insert_triple(subject, predicate, object)
     }
 
     fn curr_wrapper(&mut self, args: Option<&Sexp>) -> Ret {
@@ -417,10 +331,12 @@ impl Eval for AmlangAgent {
                             return syntax::quote(cons.cdr());
                         }
                         "def" => {
-                            return self.env_insert_node_wrapper(cons.cdr());
+                            let (name, structure) = env_insert_node_wrapper(cons.cdr())?;
+                            return Ok(self.env_insert_node(name, structure)?.into());
                         }
                         "tell" => {
-                            return self.env_insert_triple_wrapper(cons.cdr());
+                            let (s, p, o) = env_insert_triple_wrapper(cons.cdr())?;
+                            return self.env_insert_triple(s, p, o);
                         }
                         "curr" => {
                             return self.curr_wrapper(cons.cdr());
--- src/agent/mod.rs
+++ src/agent/mod.rs
@@ -1,3 +1,5 @@
 pub mod agent;
 pub mod amlang_agent;
 pub mod env_state;
+
+mod amlang_wrappers;
