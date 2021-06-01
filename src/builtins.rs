b90b6a345a6b5b7b871664d309cfe0be781d2005
Alex Khouderchah <alex.khouderchah@gmail.com>
2021-06-01 14:43:02 -0700

    builtins: Allow on-demand creation of map for bootstrapping

--- src/builtins.rs
+++ src/builtins.rs
@@ -1,8 +1,33 @@
 use std::borrow::Cow;
+use std::collections::HashMap;
 use std::convert::TryFrom;
 
 use crate::function::{Args, EvalErr, ExpectedCount, Ret};
-use crate::primitive::Number;
+use crate::primitive::{BuiltIn, Number};
+
+
+macro_rules! builtins {
+    [$($x:expr),*] => {
+        {
+            let mut m = HashMap::new();
+            $(
+                m.insert(
+                    stringify!($x),
+                    BuiltIn::new(stringify!($x), $x),
+                );
+            )*
+            m
+        }
+    };
+    [$($n:tt : $x:expr),+ ,] => {
+        builtins![$($n : $x),*]
+    };
+}
+
+// Used for bootstrapping and auxiliary purposes, not as an environment.
+pub fn generate_builtin_map() -> HashMap<&'static str, BuiltIn> {
+    builtins![add, sub, mul, div]
+}
 
 
 pub fn add(args: Args) -> Ret {
