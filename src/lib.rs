f4f7898bf79a072cd4b8bff6d2ea2dfeecc4dd1b
Alex Khouderchah <alex.khouderchah@gmail.com>
2021-07-26 13:55:01 -0700

    Make into library

---
+++ src/lib.rs
@@ -0,0 +1,40 @@
+use std::env;
+use std::path::Path;
+
+#[macro_use]
+pub mod sexp;
+
+pub mod agent;
+#[cfg(test)]
+pub mod append_vec;
+pub mod builtins;
+pub mod environment;
+pub mod function;
+pub mod model;
+pub mod parser;
+pub mod primitive;
+pub mod token;
+
+
+pub fn init() -> Result<(), String> {
+    env_logger::init();
+
+    let args: Vec<String> = env::args().collect();
+    if args.len() > 1 {
+        usage(&args);
+        return Err(format!(
+            "Wrong argument count: {}, expected 0",
+            args.len() - 1
+        ));
+    }
+
+    Ok(())
+}
+
+fn usage(args: &Vec<String>) {
+    println!(
+        "usage: {}",
+        Path::new(&args[0]).file_name().unwrap().to_string_lossy()
+    );
+    println!();
+}
