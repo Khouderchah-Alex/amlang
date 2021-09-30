9def9cafd44eb9d9db91efdaa109c6f003725e41
Alex Khouderchah <alex.khouderchah@gmail.com>
2021-09-30 12:26:58 -0700

    lib: Remove usage functionality
    Now that amlang is a lib, clients should take care of this.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 use std::env;
-use std::path::{Path, PathBuf};
+use std::path::PathBuf;
 
 #[macro_use]
 pub mod lang_err;
@@ -16,6 +16,7 @@ pub mod token;
 
 
 pub fn init(start_dir: PathBuf) -> Result<(), String> {
+    // Need to set dir to properly read relative paths in envs.
     assert!(start_dir.is_absolute());
     env::set_current_dir(start_dir).unwrap();
 
@@ -26,22 +27,5 @@ pub fn init(start_dir: PathBuf) -> Result<(), String> {
         }
     }
 
-    let args: Vec<String> = env::args().collect();
-    if args.len() > 1 {
-        usage(&args);
-        return Err(format!(
-            "Wrong argument count: {}, expected 0",
-            args.len() - 1
-        ));
-    }
-
     Ok(())
-}
-
-fn usage(args: &Vec<String>) {
-    println!(
-        "usage: {}",
-        Path::new(&args[0]).file_name().unwrap().to_string_lossy()
-    );
-    println!();
 }
