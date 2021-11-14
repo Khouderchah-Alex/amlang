1d01835691a23ddc07ae4213d6a20046eb16ad7a
Alex Khouderchah <alex.khouderchah@gmail.com>
2021-11-14 15:33:21 -0800

    cargo: Gate rustyline behind "interactive" default feature

--- src/token/mod.rs
+++ src/token/mod.rs
@@ -4,10 +4,13 @@ pub use tokenizer::TokenizeError;
 
 // Public mods.
 pub mod file_stream;
-pub mod interactive_helper;
-pub mod interactive_stream;
 pub mod string_stream;
 pub mod token;
+
+#[cfg(feature = "interactive")]
+pub mod interactive_helper;
+#[cfg(feature = "interactive")]
+pub mod interactive_stream;
 
 // Private mods.
 mod tokenizer;
