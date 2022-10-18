41db815703e05774a4fbfbd42115e12d520c75d4
Alex Khouderchah <alex.khouderchah@gmail.com>
2022-10-18 08:24:00 -0700

    primitive: builtin: Properly impl Deserialize

--- src/primitive/builtin.rs
+++ src/primitive/builtin.rs
@@ -3,9 +3,11 @@
 use std::convert::TryFrom;
 use std::fmt;
 
-use serde::{Deserialize, Serialize};
+use serde::de::{self, Deserialize, Deserializer, MapAccess, SeqAccess, Visitor};
+use serde::Serialize;
 
 use crate::agent::Agent;
+use crate::builtins::generate_builtin_map;
 use crate::error::Error;
 use crate::primitive::Primitive;
 use crate::sexp::{HeapSexp, Sexp};
@@ -13,18 +15,11 @@ use crate::sexp::{HeapSexp, Sexp};
 
 pub type Args = Vec<Sexp>;
 
-fn empty() -> fn(Args, &mut Agent) -> Result<Sexp, Error> {
-    |_: Args, _: &mut Agent| -> Result<Sexp, Error> {
-        panic!("Tried to run deserialized builtin");
-    }
-}
-
-#[derive(Clone, Serialize, Deserialize)]
+#[derive(Clone, Serialize)]
 pub struct BuiltIn {
     name: String,
 
-    // TODO(func) Use custom Deserializers to not skip.
-    #[serde(skip, default = "empty")]
+    #[serde(skip_serializing)]
     fun: fn(Args, &mut Agent) -> Result<Sexp, Error>,
 }
 
@@ -74,3 +69,90 @@ impl_try_from!(BuiltIn;
                Result<Sexp>      ->  BuiltIn,
                Result<ref Sexp>  ->  ref BuiltIn,
 );
+
+
+// Impl for generic Deserializers; custom Deserializers may want to
+// short-circuit this logic at deserialize_struct("BuiltIn",...).
+impl<'de> Deserialize<'de> for BuiltIn {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        enum Field {
+            Name,
+        }
+
+        impl<'de> Deserialize<'de> for Field {
+            fn deserialize<D>(deserializer: D) -> Result<Field, D::Error>
+            where
+                D: Deserializer<'de>,
+            {
+                struct FieldVisitor;
+
+                impl<'de> Visitor<'de> for FieldVisitor {
+                    type Value = Field;
+
+                    fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+                        formatter.write_str("`name`")
+                    }
+
+                    fn visit_str<E>(self, value: &str) -> Result<Field, E>
+                    where
+                        E: de::Error,
+                    {
+                        match value {
+                            "name" => Ok(Field::Name),
+                            _ => Err(de::Error::unknown_field(value, FIELDS)),
+                        }
+                    }
+                }
+
+                deserializer.deserialize_identifier(FieldVisitor)
+            }
+        }
+
+        struct BuiltInVisitor;
+
+        impl<'de> Visitor<'de> for BuiltInVisitor {
+            type Value = BuiltIn;
+
+            fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+                formatter.write_str("struct BuiltIn")
+            }
+
+            fn visit_seq<V>(self, mut seq: V) -> Result<BuiltIn, V::Error>
+            where
+                V: SeqAccess<'de>,
+            {
+                let name: &str = seq
+                    .next_element()?
+                    .ok_or_else(|| de::Error::invalid_length(0, &self))?;
+                // TODO(flex) Can this come from the Deserializer?
+                Ok(generate_builtin_map().get(name).unwrap().clone())
+            }
+
+            fn visit_map<V>(self, mut map: V) -> Result<BuiltIn, V::Error>
+            where
+                V: MapAccess<'de>,
+            {
+                let mut name: Option<&str> = None;
+                while let Some(key) = map.next_key()? {
+                    match key {
+                        Field::Name => {
+                            if name.is_some() {
+                                return Err(de::Error::duplicate_field("name"));
+                            }
+                            name = Some(map.next_value()?);
+                        }
+                    }
+                }
+                let name = name.ok_or_else(|| de::Error::missing_field("name"))?;
+                // TODO(flex) Can this come from the Deserializer?
+                Ok(generate_builtin_map().get(name).unwrap().clone())
+            }
+        }
+
+        const FIELDS: &'static [&'static str] = &["name"];
+        deserializer.deserialize_struct("BuiltIn", FIELDS, BuiltInVisitor)
+    }
+}
